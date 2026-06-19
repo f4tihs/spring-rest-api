588c105515bcafef8e85f876c160be56abc04c7f
Fatih Şahin <fatihsahin2855@gmail.com>
2026-06-19 13:40:05 +0300

    Fixed imports

--- src/main/java/com/quattca/repository/EmployeeRepository.java
+++ src/main/java/com/quattca/repository/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.quattca.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quattca.model.Employee;
+import com.quattca.services.EmployeeService;
 
 @Repository
 @RequestMapping("/rest/api")
