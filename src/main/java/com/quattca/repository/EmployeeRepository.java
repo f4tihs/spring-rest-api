88f19f1012fdd28540560794e6b2814d732c618c
Fatih Şahin <fatihsahin2855@gmail.com>
2026-06-19 13:35:52 +0300

    Relate GET request to method

--- src/main/java/com/quattca/repository/EmployeeRepository.java
+++ src/main/java/com/quattca/repository/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package com.quattca.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Repository
 public class EmployeeRepository {
 
+    @GetMapping(path = "/employee-list")
     public List<Employee> getAllEmployeeList(){
         return null;
     }
