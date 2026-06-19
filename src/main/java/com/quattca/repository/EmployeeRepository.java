44dc2ca38dce11c66e4b827273ad173bd8208c09
Fatih Şahin <fatihsahin2855@gmail.com>
2026-06-19 13:38:35 +0300

    Request mapping for qol and added autowire

--- src/main/java/com/quattca/repository/EmployeeRepository.java
+++ src/main/java/com/quattca/repository/EmployeeRepository.java
@@ -1,10 +1,16 @@
 package com.quattca.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Repository
+@RequestMapping("/rest/api")
 public class EmployeeRepository {
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping(path = "/employee-list")
     public List<Employee> getAllEmployeeList(){
