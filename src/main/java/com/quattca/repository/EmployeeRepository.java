bb56503682648b51e44d9f2c77a83004287103c4
Fatih Şahin <fatihsahin2855@gmail.com>
2026-06-19 13:32:41 +0300

    Basic REST API Repo for trying to remember spring

---
+++ src/main/java/com/quattca/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.quattca.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRepository {
+
+    public List<Employee> getAllEmployeeList(){
+        return null;
+    }
+}
---
+++ src/main/java/com/quattca/services/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.quattca.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+}
