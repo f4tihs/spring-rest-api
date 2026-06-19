d6d83a734c9a0b6abd3b15680a1befb8cb050812
Fatih Şahin <fatihsahin2855@gmail.com>
2026-06-19 15:49:40 +0300

    finding employees and adding etc.

--- src/main/java/com/quattca/config/AppConfig.java
+++ src/main/java/com/quattca/config/AppConfig.java
@@ -17,6 +17,8 @@ public class AppConfig {
         employeeList.add(new Employee("0", "Fatih", "Şahin"));
         employeeList.add(new Employee("1", "Yılmaz", "Yılmaz"));
         employeeList.add(new Employee("2", "Murat", "Tökeç"));
+        employeeList.add(new Employee("3", "Fatih", "Uslu"));
+
         //TODO: If only database was a thing
         return employeeList;
     }
--- src/main/java/com/quattca/repository/EmployeeRepository.java
+++ src/main/java/com/quattca/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.quattca.repository;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,49 @@ public class EmployeeRepository {
 
     public List<Employee> getAllEmployeeList(){
         return employeeList;
+    }
+
+    public Employee getEmployeeById(String id){
+        Employee foundEmployee = null;
+        // TODO: Try catch or something idk
+        for (Employee employee : employeeList) {
+            if(id.equals(employee.getId())){
+                foundEmployee = employee;
+                break;
+            }
+        }
+        return foundEmployee;
+    }
+
+    public List<Employee> getEmployeeWithParams(String firstName, String lastName){
+        List<Employee> employeeWithParams = new ArrayList<>();
+        
+        if(firstName==null && lastName==null){
+            return employeeList;
+        }
+        for (Employee employee : employeeList) {
+            if(firstName!=null && lastName!=null){
+                if(employee.getFirstName().equalsIgnoreCase(firstName) && employee.getLastName().equalsIgnoreCase(lastName)) {
+                    employeeWithParams.add(employee);
+                }
+            }
+            // FIXME: This code looks disgusting
+            if(firstName!=null && lastName==null) {
+                if(employee.getFirstName().equalsIgnoreCase(firstName)) {
+                    employeeWithParams.add(employee);
+                }
+            }
+            if(firstName==null && lastName!=null) {
+                if(employee.getLastName().equalsIgnoreCase(lastName)) {
+                    employeeWithParams.add(employee);
+                }
+            }
+        }
+        return employeeWithParams;
+    }
+
+    public Employee saveEmployee(Employee newEmployee){
+        employeeList.add(newEmployee);
+        return newEmployee;
     }
 }
